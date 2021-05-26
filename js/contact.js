16e8608b026a2513ba9b5c45466506334a55986d
Georgios Fragkias <geo.fragkias@gmail.com>
2021-05-26 12:18:22 +0200

    asdsa

--- js/contact.js
+++ js/contact.js
@@ -70,6 +70,27 @@ function formValidation(event) {
 }
 
 
+const formSubmissionHandler = (event) => {
+    event.preventDefault();
+  
+    const formElement = event.target,
+      { action, method } = formElement,
+      body = new FormData(formElement);
+  
+    fetch(action, {
+      method,
+      body
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+  const formElement = document.querySelector("form");
+  
+  formElement.addEventListener("submit", formSubmissionHandler);
+
 /*------------------------   Clear Document Function -----------------------------*/
 
 document.addEventListener('mouseup', clear)
